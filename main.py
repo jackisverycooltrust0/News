a7f4636a109853002424288d0abf50ffb69e7c8a
jackisverycooltrust0 <richnguyengia@gmail.com>
2025-04-27 10:53:14 +0700

    reinitialize

--- main.py
+++ main.py
@@ -24,15 +24,3 @@ def index():
 if __name__ == "__main__":                                                                                                                                  
     app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
                                                                                                                            
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
